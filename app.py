44c8588f9031649b5f942d27c67ad4899ebafa1e
derickgabrielschumacherdealmei-stack <derickgabrielschumacherdealmei@gmail.com>
2026-09-19 14:48:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
-olá! o meu assistente de ia está on line e funcionando . 
+print("Bem-vindo ao assistente de IA do Derick!")
+
+pergunta_usuario = input("Digite alguma coisa para conversar: ")
+
+print("Você disse:", pergunta_usuario)
